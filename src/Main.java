0cb35e2fc0a151936252e0f83f10e6cf45d53ed4
Ja1r117 <jairbhodoo@gmail.com>
2026-04-26 23:05:59 -0300

    Delete src directory

--- src/Main.java
+++
@@ -1,8 +0,0 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-    System.out.println("Hello");
-    System.out.println("jkk");
-
-    }
-
